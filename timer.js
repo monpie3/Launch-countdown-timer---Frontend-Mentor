e063c2dd78d2cf78ec1ca8dd1f155be07aa811e1
Monika <dotyk_zla@o2.pl>
2020-12-11 22:25:32 +0100

    Style counter cards

--- timer.js
+++ timer.js
@@ -3,16 +3,23 @@ var target_date = new Date("Jan 1, 2021 00:00:00").getTime();
 function countdown()
 	{
         var today = new Date().getTime();
-
         var distance = target_date - today;
 		
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-		
-        document.getElementById("zegar").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+
+        if (days<10) days = "0"+days;
+        if (hours<10) hours = "0"+hours;
+        if (minutes<10) minutes = "0"+minutes;
+        if (seconds<10) seconds = "0"+seconds;
+        
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+        
 		 
         if (distance > 0) {
             setTimeout("countdown()",1000);
