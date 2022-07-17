54b4aecb9b1dfe7478b5c1a8a520c4f7ceafe4b7
Monika <dotyk_zla@o2.pl>
2022-07-17 18:13:24 +0200

    timer resets every year

--- timer.js
+++ timer.js
@@ -1,23 +1,25 @@
-var target_date = new Date("Jan 1, 2022 00:00:00").getTime();
+const target_year = new Date().getFullYear() + 1;  
+const target_date = new Date(`Jan 1, ${target_year} 00:00:00`);
+
 
 function countDown() {
-    let today = new Date().getTime();
-    let distance = target_date - today;
+    const today = new Date().getTime();
+    const distance = target_date - today;
 		
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    if (days<10) days = "0"+days;
-    if (hours<10) hours = "0"+hours;
-    if (minutes<10) minutes = "0"+minutes;
-    if (seconds<10) seconds = "0"+seconds;
+    if (days<10) days = "0" + days;
+    if (hours<10) hours = "0" + hours;
+    if (minutes<10) minutes = "0" + minutes;
+    if (seconds<10) seconds = "0" + seconds;
 
-    document.getElementById("seconds").setAttribute("data-content",seconds);
-    document.getElementById("minutes").setAttribute("data-content",minutes);
-    document.getElementById("hours").setAttribute("data-content",hours);
-    document.getElementById("days").setAttribute("data-content",days);
+    document.getElementById("seconds").setAttribute("data-content", seconds);
+    document.getElementById("minutes").setAttribute("data-content", minutes);
+    document.getElementById("hours").setAttribute("data-content", hours);
+    document.getElementById("days").setAttribute("data-content", days);
         
 
     if (distance > 0) {
@@ -31,7 +33,7 @@ function countDown() {
     }
 
     else {
-        document.getElementsByClassName("wrapper")[0].innerHTML = "<h1>Goodbye 2021, Hello 2022!</h1>";
+        document.getElementsByClassName("wrapper")[0].innerHTML = `<h1> Goodbye ${target_year-1}, Hello ${target_year}! </h1>`;
     }     
 }
     
